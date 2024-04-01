f5b07da98dad7d0275e3645e4e8887d475b3b4a0
Tomide Adeoye <55337742+tomideadeoye@users.noreply.github.com>
2024-04-01 15:27:51 +0100

    sdk push

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Base, Movies } from "./movies";
-declare class TomideStreams extends Base {
-}
-interface TomideStreams extends Movies {
-}
-export default TomideStreams;
-//# sourceMappingURL=index.d.ts.map
