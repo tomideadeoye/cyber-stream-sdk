cfb8114b5a8e128c5c23c2ceca4bf57f994871bd
Tomide Adeoye <55337742+tomideadeoye@users.noreply.github.com>
2024-04-01 22:10:35 +0100

    remove client

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,4 @@ interface TomideStreams extends Movies {}
 
 applyMixins(TomideStreams, [Movies]);
 
-const client = new TomideStreams();
-
-const randomMovies = async () => {
-  const movies = await client.getRandomMovies();
-  console.log(movies);
-};
-
-randomMovies();
-
 export default TomideStreams;
